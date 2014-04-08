9779c694164a9255a6ad1afa1d58ffd2b1be9fbf
Fredric <fredric.berling@gmail.com>
2014-04-08 22:16:59 +0200

    Updated Sencha Cmd

--- public/clients/Desktop/app/model/User.js
+++ public/clients/Desktop/app/model/User.js
@@ -2,11 +2,11 @@ Ext.define('BIFF.model.User', {
     extend: 'Ext.data.Model',
     idProperty: '_id',
     fields: [
-        {   name: '_id'              , type: 'string'         },
-        {   name: 'username'         , type: 'string'         },
-        {   name: 'email'            , type: 'string'         },
-        {   name: 'password'         , type: 'string'         },
-        {   name: 'facebook'         , type: 'auto'           }
+        {   name: '_id'              , type: 'string', useNull:true     },
+        {   name: 'username'         , type: 'string'                   },
+        {   name: 'email'            , type: 'string'                   },
+        {   name: 'password'         , type: 'string'                   },
+        {   name: 'facebook'         , type: 'auto'                     }
     ],
     proxy: {
         type: 'rest',
