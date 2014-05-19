914909c827b4709bc97d2ff620058124428a18a8
Fredric <fredric.berling@gmail.com>
2014-05-19 23:07:00 +0200

    Changed to use viewcontrollers

---
+++ public/clients/Desktop/app/view/login/Login.js
@@ -0,0 +1,51 @@
+Ext.define('BIFF.view.login.Login', {
+    extend: 'Ext.form.Panel',
+    controller: 'logincontroller',
+    xtype: 'login',
+    layout: {
+        type: 'vbox',
+        align: 'center',
+        pack: 'center'
+    },
+    buttonAlign: 'left',
+    border: false,
+    defaults: {
+        xtype: 'textfield',
+        hideLabel: true
+    },
+    items : [
+        {
+            name: 'username',
+            ui:'login',
+            enableKeyEvents:true,
+            maxLength: 60,
+            allowBlank:false,
+            emptyText: 'Användarnamn'
+         },
+        {
+            emptyText: 'Lösenord',
+            allowBlank:false,
+            ui:'login',
+            enableKeyEvents:true,
+            name: 'password',
+            maxLength: 60,
+            inputType: 'password'
+        },
+        {
+            xtype: 'button',
+            itemId:'loginbutton',
+            formBind:true,
+            scale: 'medium',
+            text: 'Login'
+        },
+        {
+            xtype: 'button',
+            itemId:'passreset',
+            scale: 'small',
+            text: 'Forgot password'
+        }
+    ]
+
+
+
+});
