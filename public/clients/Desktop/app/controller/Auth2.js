f9ed201bf8e64390b06231ce47c48d2130f706f3
Fredric <fredric.berling@gmail.com>
2014-05-21 21:52:35 +0200

    Finally got a working viewmodel

---
+++ public/clients/Desktop/app/controller/Auth2.js
@@ -0,0 +1,59 @@
+Ext.define('BIFF.controller.Auth', {
+    extend: 'Ext.app.Controller',
+    config : {
+        routes : {
+            'auth/login'                 : 'showLogin',
+            'auth/changepassword/:token' : 'showChangePassword',
+            'auth/reset'                 : 'showPassReset'
+        }
+    },
+    views: [
+        'login.Login',
+        'login.PassReset',
+        'login.PassChange',
+
+    ],
+
+
+    refs: [
+        {
+            ref: 'login',
+            selector: 'login',
+            xtype:'login'
+        },
+        {
+            ref: 'passChange',
+            selector: 'passchange',
+            autoCreate: true,
+            xtype:'passchange'
+        },
+        {
+            ref: 'passReset',
+            selector: 'passreset',
+            autoCreate: true,
+            xtype: 'passreset'
+        }
+    ],
+
+    showLogin: function (params) {
+        var viewport = Ext.ComponentQuery.query('#bodycardpanel')[0];
+
+        var login = Ext.create('BIFF.view.login.Login',{
+            session : BIFF.loginManager.getSession()
+        });
+
+        viewport.layout.setActiveItem(viewport.add(login));
+    },
+    showChangePassword: function (token) {
+        var viewport = Ext.ComponentQuery.query('#bodycardpanel')[0];
+        viewport.layout.setActiveItem(viewport.add(this.getPassChange()));
+        this.getPassChange().getForm().findField('token').setValue(token);
+    },
+    showPassReset: function () {
+        var viewport = Ext.ComponentQuery.query('#bodycardpanel')[0];
+
+        viewport.layout.setActiveItem(viewport.add(this.getPassReset()));
+    }
+
+
+});
